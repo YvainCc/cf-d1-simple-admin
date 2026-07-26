5266befebd9d5f22f6407302320be8ed8c3169e2
YvainCc <yvainchuang@outlook.com>
2026-07-26 12:34:58 +0800

    Update login.js
    修改登录逻辑

--- functions/api/login.js
+++ functions/api/login.js
@@ -1,8 +1,13 @@
+import { SignJWT } from 'jose';
+// 【务必自行修改为随机长密钥，不要泄露】
+const JWT_SECRET = new TextEncoder("DMN2026_SecretKey_99887766");
+const TOKEN_EXPIRE_HOUR = 168; // 7天有效期
+
 export async function onRequest({ request, env }) {
   const corsHeaders = {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "POST,OPTIONS",
-    "Access-Control-Allow-Headers": "Content-Type"
+    "Access-Control-Allow-Headers": "Content-Type,Authorization"
   };
 
   if (request.method === "OPTIONS") {
@@ -24,17 +29,35 @@ export async function onRequest({ request, env }) {
         .all();
 
       if (sqlRes.results.length > 0) {
+        const userInfo = sqlRes.results[0];
+        // 生成JWT Token
+        const token = await new SignJWT({
+          username: username,
+          role: user.role
+        })
+          .setIssuedAt()
+          .setExpiration(`${TOKEN_EXPIRE_HOUR}h`)
+          .sign(JWT_SECRET);
+        
+        // 计算过期时间存入数据库
+        const expireTime = new Date(Date.now() + TOKEN_EXPIRE_HOUR * 3600 * 1000).toISOString();
+        await env.DB.prepare(`
+          INSERT INTO login_token(username,token,expire_at) VALUES (?,?,?)
+        `).bind(username, token, expireTime).run();
+
         return Response.json({
           ok: true,
           msg: "登录成功",
-          role: sqlRes.results[0].role
+          role: user.role,
+          token, // 新增令牌返回前端
+          username
         }, { headers: corsHeaders });
       } else {
         return Response.json({ ok: false, msg: "账号或密码错误" }, { headers: corsHeaders });
       }
     }
     return Response.json({ ok: false, msg: "非法请求" }, { headers: corsHeaders });
-  } catch (err) {
+  } catch (err)
     return Response.json({ ok: false, msg: "服务异常：" + err.message }, { status: 500, headers: corsHeaders });
   }
 }
